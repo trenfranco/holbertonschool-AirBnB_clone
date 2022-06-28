7e8654095069dde88038ed32c2a1e7be0d8e3095
trenfranco <trenfranco@gmail.com>
2022-06-28 12:47:23 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,15 @@ import datetime
 class BaseModel:
     """Base class"""
     def __init__(self):
-        BaseModel.id = str(uuid.uuid4())
-        BaseModel.created_at = datetime.datetime.now()
-        BaseModel.updated_at = datetime.datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
     def __str__(self):
-        return(f"[BaseModel] ({self.id}) {self.to_dict()}")
+        return(f"[BaseModel] ({self.id}) {self.__dict__}")
     def save(self):
         BaseModel.updated_at = datetime.datetime.now()
     def to_dict(self):
-        new = {'__class__': __class__.__name__, 'updated_at': self.updated_at.isoformat(), 'id': self.id, 'created_at': self.created_at.isoformat()}
+        new = {'__class__': __class__.__name__}
         new2 = {**self.__dict__, **new}
         return new2
 
