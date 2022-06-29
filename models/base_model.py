9dd6b9c371ce2f80f26e09949316a6de7da941ca
MauriPercovich <mauriciopercovich22@gmail.com>
2022-06-29 14:03:29 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,15 @@ import datetime
 
 class BaseModel:
     """Base class"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
     def __str__(self):
         return(f"[BaseModel] ({self.id}) {self.__dict__}")
     def save(self):
@@ -18,5 +23,3 @@ class BaseModel:
         new = {'__class__': __class__.__name__}
         new2 = {**self.__dict__, **new}
         return new2
-
-
