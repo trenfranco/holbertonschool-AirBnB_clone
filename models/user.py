ad615f7492014a7cc7def8e5a0b3cc690b125090
MauriPercovich <mauriciopercovich22@gmail.com>
2022-07-01 11:47:25 -0700

    user

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+usr
+"""
+from models.Base_model import BaseModel
+
+class User(BaseModel):
+    """
+    usr
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
