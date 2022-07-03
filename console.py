e334626a57571a34cd9f21b5e231e45a16c5b0b3
MauriPercovich <mauriciopercovich22@gmail.com>
2022-07-03 15:22:45 -0700

    fixxx

--- console.py
+++ console.py
@@ -14,6 +14,8 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import json
+import ast
 
 
 class HBNBCommand(cmd.Cmd):
