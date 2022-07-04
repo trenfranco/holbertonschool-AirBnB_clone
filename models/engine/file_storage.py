ff8bfb5998017688b5ef0dbee55adc5b6a928bc5
MauriPercovich <mauriciopercovich22@gmail.com>
2022-07-03 17:43:33 -0700

    5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,12 @@ and deserializes JSON file to instances
 import json
 import os
 from models.base_model import BaseModel
-
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
